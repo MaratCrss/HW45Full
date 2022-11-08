33b63b3268758d134b3d7def0c77f329a9932844
Marat <marat.mashanlo@gmail.com>
2022-11-08 17:47:56 +0600

    rabotay so ssylkami

--- webapp/views.py
+++ webapp/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from webapp.models import Task, STATUS_CHOICES
+from django.http import HttpResponseRedirect
 # Create your views here.
 
-def index_view(request):
+def index_view(request, *args, **kwargs):
     if request.method == "POST":
         task_id = request.GET.get('id')
         task = Task.objects.get(id=task_id)
@@ -10,7 +11,7 @@ def index_view(request):
     task = Task.objects.all()
     return render(request, 'index.html', {'tacks': task})
 
-def create_task(request):
+def create_task(request, *args, **kwargs):
     if request.method == "GET":
         return  render(request, 'create.html', {'statuses': STATUS_CHOICES})
     elif request.method == "POST":
@@ -19,5 +20,12 @@ def create_task(request):
         deadline = request.POST.get('deadline')
         content = request.POST.get('content')
         new_task = Task.objects.create(title=title, status=status, content=content, deadline=deadline)
-        return render(request, 'task_view.html', {'task': new_task})
+        #return render(request, 'task_view.html', {'task': new_task})
+        return HttpResponseRedirect(f'/task/?id={new_task.pk}')
+
+def task_view(request, *args, **kwargs):
+    task_id = request.GET.get('id')
+    task = Task.objects.get(pk=task_id)
+    context = {'task': task}
+    return render(request, 'task_view.html', context)
 
